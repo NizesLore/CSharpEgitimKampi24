3c5bf22d297a635ef067744e24226432c823e196
Sezin EROL <fenciszn@hotmail.com>
2024-10-20 21:01:03 +0300

    If else

--- 03_MakingDecision/Program.cs
+++ 03_MakingDecision/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,113 @@ namespace _03_MakingDecision
     {
         static void Main(string[] args)
         {
-            
+            #region If Else
+
+            //Console.WriteLine("Lütfen şifreyi giriniz: ");
+
+            //string password;
+            //password = Console.ReadLine();
+
+            //if (password == "abcd") {
+
+            //    Console.WriteLine("Şifre doğru");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifreyi yanlış girdiniz !");
+            //}
+
+            //string capital, country;
+
+            //Console.Write("Başkenti giriniz: ");
+            //capital = Console.ReadLine();
+
+
+            //Console.Write("Ülkeyi giriniz: ");
+            //country= Console.ReadLine();
+
+
+            //if (capital =="ankara" & country=="türkiye")
+            //{
+            //    Console.WriteLine("Veriler doğrulandı");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hatalı bilgi");
+            //}
+
+            //int number;
+            //Console.Write("Sayıyı giriniz: ");
+            //number=int.Parse(Console.ReadLine());
+            //if (number==5)
+            //{
+            //    Console.WriteLine("Sayı doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı hatalı");
+            //}
+
+            //int exam1, exam2, exam3, average;
+            //string result="Hata !";
+
+            //Console.Write("Sınav1: ");
+            //exam1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Sınav2: ");
+            //exam2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Sınav3: ");
+            //exam3 = int.Parse(Console.ReadLine());
+
+            //average = (exam1 + exam2 + exam3) / 3;
+            //Console.WriteLine("Sınavların ortalaması: " + average);
+
+            //if (average > 0 & average <= 50)
+            //{
+            //    result = "Sonuç düşük";
+
+            //}
+            //if (average > 50 & average <= 70)
+            //{
+            //    result = "Sonuç orta";
+            //}
+            //if (average > 70 & average <= 84)
+            //{
+            //    result = "Sonuç iyi";
+
+            //}
+            //if (average > 84)
+            //{
+            //    result = "Sonuç çok iyi";
+            //}
+
+            //Console.WriteLine(result);
+
+
+            //string city;
+            //Console.Write("Lütfen şehir girişi yapınız: ");
+            //city = Console.ReadLine();
+
+
+            //if (city == "adana" | city == "ankara" | city == "bursa" | city == "trabzon")
+            //{
+            //    Console.WriteLine("Şehir bulunmaktadır");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şehir bulunmamaktadır");
+            //}
+
+
+
+
+            Console.Read();
+
+
+            #endregion
+
         }
     }
 }
