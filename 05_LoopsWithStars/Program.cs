a7bbf04fc571b9bf5b0be1247dcf0c80a287fcec
Sezin EROL <fenciszn@hotmail.com>
2024-11-03 15:01:54 +0300

    .

--- 05_LoopsWithStars/Program.cs
+++ 05_LoopsWithStars/Program.cs
@@ -10,6 +10,128 @@ namespace _05_LoopsWithStars
     {
         static void Main(string[] args)
         {
+            #region Alt Alta 10 Tane Yıldız Oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan Yana 10 Tane Yıldız Oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion;
+
+            #region Alt Alta 10 Tane Yıldız Oluşturma Her Satırda 10 Tane Yıldız Olsun
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+
+
+            #endregion
+
+            #region Dik Ucgen
+
+            //for (int i = 1; i <=5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+            #region Ters Dik Ucgen
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            #endregion
+
+            #region Dik ve Ters Dik Ucgen Beraber
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int m = 4; m >= 1; m--)
+            //{
+            //    for (int k = 1; k <= m; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Dilimi
+
+            //Ust Kısım
+
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+            //Alt Kısım
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            #endregion
+
+            Console.Read();
+
+
         }
     }
 }
