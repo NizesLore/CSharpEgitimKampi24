fb711a5a2919d84cc3541da030101fe2af08e858
Sezin EROL <fenciszn@hotmail.com>
2024-11-04 22:38:50 +0300

    colors

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -15,6 +15,18 @@ namespace _06_Arrays
             //2 4 6 8 
             //sarı, kırmızı, mavi, turuncu, beyaz
             //adana, ankara, istanbul, bursa
+            //DegiskenTuru[] DiziAdi =new DegiskenTuru[ElemanSayisi]
+
+            string[] colors = new string[4];
+            colors[0] = "Kırmızı";
+            colors[1] = "Sarı";
+            colors[2] = "Beyaz";
+            colors[3] = "Mavi";
+
+            Console.WriteLine(colors[2]);
+
+
+
 
             #endregion
 
