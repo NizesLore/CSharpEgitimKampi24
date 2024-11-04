42b09350f71fc5e2fcc53c25f89103bee35e824f
Sezin EROL <fenciszn@hotmail.com>
2024-11-04 22:28:34 +0300

    Loops

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Temel Dizi Ornekleri
+
+            //2 4 6 8 
+            //sarı, kırmızı, mavi, turuncu, beyaz
+            //adana, ankara, istanbul, bursa
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
