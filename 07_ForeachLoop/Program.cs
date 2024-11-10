607219a74aa295c67cbf6f79a30fe29cfdbfb3c3
Sezin EROL <fenciszn@hotmail.com>
2024-11-10 22:57:04 +0300

    List<>

--- 07_ForeachLoop/Program.cs
+++ 07_ForeachLoop/Program.cs
@@ -43,15 +43,29 @@ namespace _07_ForeachLoop
             //    }
             //}
             //Console.WriteLine("-----------------------");
-            int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
+            //int[] numbers = { 45, 78, 985, 635, 74, 11, 22, 33, 41, 205, 6578, 10394 };
 
-            int total = 0;
-            foreach (int i in numbers)
+            //int total = 0;
+            //foreach (int i in numbers)
+            //{
+            //    total += i;
+
+            //}
+            //Console.WriteLine(total);
+            //Console.WriteLine("-----------------------");
+
+            List<int> numbers = new List<int>()
             {
-                total += i;
+                1,2,3,4,5,8
 
+
+            };
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
             }
-            Console.WriteLine(total);
+
 
 
             #endregion
