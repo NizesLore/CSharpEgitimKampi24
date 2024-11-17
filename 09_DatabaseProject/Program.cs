556eaeed025b9d606f1b5fb6f75cf506616a6f73
Sezin EROL <fenciszn@hotmail.com>
2024-11-17 21:30:41 +0300

    SqlConnection

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,18 @@ namespace _09_DatabaseProject
     {
         static void Main(string[] args)
         {
+
+            string tableNumber;
             Console.WriteLine("-----------------------");
             Console.WriteLine("1-Kategoriler");
             Console.WriteLine("2-Ürünler");
             Console.WriteLine("3-Siparişler");
             Console.WriteLine("4-Çıkış Yap");
             Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
-
-
-
-
-
-
-
-
+            tableNumber=Console.ReadLine();
             Console.WriteLine("-----------------------");
 
-
+            //SqlConnection connection = new SqlConnection(tableNumber);
 
 
             Console.Read();
