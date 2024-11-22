f46040182b85842856c9c03d769221fb91f37559
Sezin EROL <fenciszn@hotmail.com>
2024-11-22 21:32:36 +0300

    .

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -61,7 +61,7 @@ namespace _09_DatabaseProject
             SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)", connection);
             command.Parameters.AddWithValue("@p1",categoryName);
             command.ExecuteNonQuery();
-
+            connection.Close();
 
             Console.Read();
         }
