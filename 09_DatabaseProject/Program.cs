ceb8513d2c785ed352cdbd98b14e9c258fc2841f
Sezin EROL <fenciszn@hotmail.com>
2025-01-18 10:52:28 +0300

    .

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -176,21 +176,8 @@ namespace _09_DatabaseProject
             //Console.WriteLine("Güncelleme başarılı! ");
             #endregion
 
-            
-            Console.WriteLine();
-            Console.WriteLine();
 
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-
-            Console.WriteLine();
-
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine( );
 
             Console.Read();
         }
