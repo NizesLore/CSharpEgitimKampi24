d840e22e06d49ef472d86ec5301473c051ce3eef
Sezin EROL <fenciszn@hotmail.com>
2025-07-21 22:18:56 +0300

    .

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -189,6 +189,8 @@ namespace _09_DatabaseProject
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine();
+
 
 
             Console.Read();
