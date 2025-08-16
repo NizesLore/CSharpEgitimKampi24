2d396588875d81ebf37ca93ddb0aac80b3dca281
Sezin EROL <fenciszn@hotmail.com>
2025-08-16 09:22:26 +0300

    .

--- 10_DatabaseCrud1/Program.cs
+++ 10_DatabaseCrud1/Program.cs
@@ -13,7 +13,7 @@ namespace _10_DatabaseCrud1
         static void Main(string[] args)
         {
             //Crud--> Create-Read-Update-Delete
-            
+
 
             Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
             Console.WriteLine();
@@ -61,20 +61,24 @@ namespace _10_DatabaseCrud1
             Console.Write("Ürün Adı: ");
             productName = Console.ReadLine();
             Console.Write("Ürün Fiyatı: ");
-            productPrice=decimal.Parse(Console.ReadLine());
-            
+            productPrice = decimal.Parse(Console.ReadLine());
+
             SqlConnection connection = new SqlConnection("Data source=(localdb)\\MSSQLLocalDB; initial catalog=EgitimKampiDb;integrated security=true");
             connection.Open();
             SqlCommand command = new SqlCommand("insert into TblProduct (ProductName, ProductPrice,ProductStatus) values (@productName,@productPrice,@productStatus)", connection);
             command.Parameters.AddWithValue("@productName", productName);
-            command.Parameters.AddWithValue("@productPrice",productPrice);
+            command.Parameters.AddWithValue("@productPrice", productPrice);
             command.Parameters.AddWithValue("@productStatus", true);
             command.ExecuteNonQuery();
             connection.Close();
             Console.Write("Ürün eklemesi başarılı!");
 
-
             #endregion
+
+
+
+
+
             Console.Read();
 
 
