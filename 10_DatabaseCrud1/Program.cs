d7ed5a614079bff3a57a6a9fef024bf6209bb510
Sezin EROL <fenciszn@hotmail.com>
2026-02-15 23:38:09 +0300

    .

--- 10_DatabaseCrud1/Program.cs
+++ 10_DatabaseCrud1/Program.cs
@@ -160,7 +160,8 @@ namespace _10_DatabaseCrud1
             #endregion
 
              
-            Console.Read();                   
+            Console.Read();           
+              
               
 
               
